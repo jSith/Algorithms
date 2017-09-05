dada9556f8891444f5db0ccf20738b72042aa2fb
jSith <30677679+jSith@users.noreply.github.com>
2017-09-05 18:57:09 -0500

    Update hw2_3.py

--- hw2_3.py
+++ hw2_3.py
@@ -12,9 +12,31 @@ def max_sum (A, index, sum, max):
         return max_sum(A, index+1, sum+A[index], max)
 
 
+def prefix (A, start, end):
+
+    # Input: A, a list of integers; start, the starting index; end, the ending index;
+        # sum, a running total of the sum of A's elements; max, the max prefix sum of A
+    # Output: The largest prefix sum in A and the sum of the whole list
+
+    # Base case: length is 1
+    if start == end:
+        return A[start], A[start]
+
+    # Split in 2
+    midpoint = math.floor((start+end)/2)
+    left = prefix(A, start, midpoint)
+    right = prefix(A, midpoint+1, end)
+
+    # Find sum + prefix for both halves
+    if left[0] > right[0] + left[1]:
+        return left[0], left[1] + right[1]
+    else:
+        return right[0]+left[1], left[1] + right[1]
+
 def main():
     A = [6, 4, -2, 3, -5, 1]
     print("Expected:", max_sum(A, 0, 0, 0))
+    print("Actual:", prefix(A, 0, len(A)-1))
 
 if __name__ == '__main__':
     main()
