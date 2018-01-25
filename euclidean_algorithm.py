57bccfc69220429b0e8605580b2d1e24bf17a2c3
Jessica Smith <30677679+jSith@users.noreply.github.com>
2018-01-25 02:44:53 -0600

    Add files via upload
    Added random linear combination method

--- euclidean_algorithm.py
+++ euclidean_algorithm.py
@@ -1,24 +1,42 @@
 import sys
+import random
+
 
 def euclidean_algorithm (a, b):
 
-    r = 1
+    remainder = 1
 
-    while r != 0:
+    while remainder != 0:
         if a < b:
             a = a + b
             b = a - b
             a = a - b
-        r = a % b
-        if r == 0:
-            return b
+        remainder = a % b
         a = b
-        b = r
+        b = remainder
 
-    return b
+    print(a)
+    return a
+
+
+def linear_combination(d, a, b):
+
+    combo = 0
+    x = -10
+    y = 1
+
+    while combo != d:
+        y = random.randint(-100, 100)
+        x = random.randint(-100, 100)
+        combo = x * a + y * b
+
+    return x, y
+
 
 def main():
-    print(euclidean_algorithm(int(sys.argv[1]), int(sys.argv[2])))
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    print(linear_combination(euclidean_algorithm(a, b), a, b))
 
 if __name__ == '__main__':
     main()
